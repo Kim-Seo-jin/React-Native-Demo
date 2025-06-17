65213966057fe6e7c3a7def6c6881b4e3dcade40
Kim-Seo-jin <k.seojinn@gmail.com>
2025-06-17 21:56:12 +0900

    first commit

---
+++ NaverWebViewApp/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import ShoppingScreen from './screens/ShoppingScreen';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Shopping" component={ShoppingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ NaverWebViewApp/screens/ShoppingScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ShoppingScreen = () => {
+    return (
+        <View>
+            <Text>Shopping</Text>
+        </View>
+    )
+}
+
+export default ShoppingScreen;
